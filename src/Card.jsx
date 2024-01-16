a861199f1e6201df5069e262893e69d771e93451
Adam <adammiller.2000nj@gmail.com>
2024-01-16 14:48:11 -0500

    added files from tutorial

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return(
+        <div className="card">
+            <img className="card-img" src="https://pbs.twimg.com/profile_images/1690954306214662144/zXxHDeDt_400x400.jpg" alt="Profile Picture"></img>
+            <h2 className="card-title">Adam</h2>
+            <p className="card-text">I'm a web developer and play video games</p>
+        </div>
+    );
+}
+
+export default Card;
