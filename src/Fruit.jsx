e9c61057ed2e855d176844aa26eb78deb1a9eb27
Adam <adammiller.2000nj@gmail.com>
2024-02-21 14:22:55 -0500

    rendered array of objects

---
+++ src/Fruit.jsx
@@ -0,0 +1,6 @@
+export default function Fruit() {
+
+    return(
+        <div>Fruit</div>
+    )
+};
--- src/Fruits.jsx
+++ src/Fruits.jsx
@@ -1,11 +1,17 @@
 export default function Fruits() {
 
-    const fruits = ['Apple', 'Mango', 'Banana', 'Orange', 'Pineapple'];
+    const fruits = [
+        { name: "Apple", price: 10, emoji: "🍎" },
+        { name: "Mango", price: 7, emoji: "🥭" },
+        { name: "Banana", price: 2, emoji: "🍌" },
+        { name: "Orange", price: 5, emoji: "🍊" },
+        { name: "Pineapple", price: 8, emoji: "🍍" },
+    ];
 
     return(
     <div>
         <ul>
-            {fruits.map((f) => <li key={f}>{f}</li>)}
+            {fruits.map((f) => <li key={f.name}>{f.emoji} {f.name} ${f.price}</li>)}
         </ul>
     </div>
     )
