f13a261132abadcc48a546ed6af02ee9c8029ff3
Adam <adammiller.2000nj@gmail.com>
2024-02-25 14:12:06 -0500

    updated from YT tutorial (Conditional rendering with variables)

--- src/ConditionalComponent.jsx
+++ src/ConditionalComponent.jsx
@@ -1,12 +1,11 @@
-import Welcome from './Welcome';
-import Code from './Code';
-
 export default function ConditionalComponent() {
+    let message;
     const display = true;
-
     if(display){
-        return <Welcome/>
+        message = <h1>This is message 1</h1>;
     } else {
-        return <Code/>
+        message = <h1>This is message 2</h1>;
     }
+
+    return message;
 };
