915928ff7d115a2b8bb68aa796b8553997c2e5f7
Adam <adammiller.2000nj@gmail.com>
2024-02-25 14:18:14 -0500

    altered conditionalcomponent to a ternary operator

--- src/ConditionalComponent.jsx
+++ src/ConditionalComponent.jsx
@@ -1,11 +1,5 @@
 export default function ConditionalComponent() {
-    let message;
     const display = true;
-    if(display){
-        message = <h1>This is message 1</h1>;
-    } else {
-        message = <h1>This is message 2</h1>;
-    }
-
-    return message;
+    
+    return(display ? <h1>Message 1</h1> : <h1>Message 2</h1>);
 };
