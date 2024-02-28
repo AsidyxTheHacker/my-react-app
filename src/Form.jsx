9d075120c9cc96e9052728d6e6d4492b9add7bcf
Adam <adammiller.2000nj@gmail.com>
2024-02-28 14:30:52 -0500

    handling multiple input fields with objects

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,17 +2,25 @@ import React, { useState } from 'react';
 
 export default function Form() {
 
-    const [name, setName] = useState("");
-
-    function handleChange(e) {
-        setName(e.target.value)
-    }
+    const [person, setPerson] = useState({ firstName: "", lastName: "", age: 0 });
 
     return(<>
     <div>
         <form>
-            <input onChange={handleChange} type="text" value={name} placeholder="Name"/>
-            <h1>Hello, {name}</h1>
+            <input onChange={(e) => setPerson({...person, firstName: e.target.value})} 
+                type="text" 
+                value={person.firstname} 
+                placeholder="First Name" />
+            <input onChange={(e) => setPerson({...person, lastName: e.target.value})} 
+                type="text" 
+                value={person.lastName} 
+                placeholder="Last Name" />
+            <input onChange={(e) => setPerson({...person, age: e.target.value})} 
+                type="number" 
+                value={person.age} 
+                placeholder="Age" />
+            <h1>Hello, {person.firstName} {person.lastName}.</h1>
+            <h2>You are {person.age} years old</h2>
         </form>
     </div>
     </>)
