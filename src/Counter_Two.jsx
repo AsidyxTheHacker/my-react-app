e367bbf25e9ab7f2b94c35061a59ad98b37248bc
Adam <adammiller.2000nj@gmail.com>
2024-02-28 13:55:53 -0500

    added multiple states

--- src/Counter_Two.jsx
+++ src/Counter_Two.jsx
@@ -2,13 +2,22 @@ import React, { useState } from 'react';
 
 export default function Counter_Two() {
     const [count, setCount] = useState(0);
+    const [incrementBy, setIncrementBy] = useState(1);
 
     function handleClickAdd() {
-        setCount((c) => c + 1)
+        setCount((c) => c + incrementBy);
     }
 
     function handleClickSub() {
-        setCount((c) => c - 1)
+        setCount((c) => c - incrementBy);
+    }
+
+    function increaseIncrement() {
+        setIncrementBy((i) => i + 1);
+    }
+
+    function decreaseIncrement() {
+        setIncrementBy((i) => i - 1);
     }
 
     return(<>
@@ -16,6 +25,10 @@ export default function Counter_Two() {
             <h1>Count Value is: {count}</h1>
             <button onClick={handleClickAdd}>Add</button>
             <button onClick={handleClickSub}>Sub</button>
+
+            <h1>We are incremementing the value by: {incrementBy}</h1>
+            <button onClick={increaseIncrement}>Increase</button>
+            <button onClick={decreaseIncrement}>Decrease</button>
         </div>
     </>)
 };
